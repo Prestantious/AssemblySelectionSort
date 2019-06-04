f97881f2eb1ffab52e1a13e7762bdae8ad311ca4
Dominic Daprano <40330764+Prestantious@users.noreply.github.com>
2019-06-03 20:52:46 -0700

    Update selectionSort.asm
    Changed some of the formattings of the file to make it easier to read. (Lined up comments and function calls)

--- selectionSort.asm
+++ selectionSort.asm
@@ -41,7 +41,7 @@ main PROC	; (insert executable instructions here)
 
 	call	intro				; prints out inroductary message
 
-	push	OFFSET numRand		; pushes the variable that we want getData to read user input to
+	push	OFFSET numRand			; pushes the variable that we want getData to read user input to
 	call	getData				; gets the user input for the ammount of numbers that they would like to output
 
 
@@ -52,7 +52,7 @@ main PROC	; (insert executable instructions here)
 	push	OFFSET nsorted
 	push	OFFSET list
 	push	numRand
-	call	displayArray		; prints out array after filling it
+	call	displayArray			; prints out array after filling it
 
 	push	OFFSET list
 	push	numRand
@@ -65,9 +65,9 @@ main PROC	; (insert executable instructions here)
 	push	OFFSET sorted
 	push	OFFSET list
 	push	numRand
-	call	displayArray		; prints out array after sorting it
+	call	displayArray			; prints out array after sorting it
 
-	exit	; exit to operating system
+	exit					; exit to operating system
 main ENDP
 
 ;
@@ -77,17 +77,17 @@ main ENDP
 ; Post -> the welcome message is printed to the user
 ;
 intro PROC
-	mov		edx, OFFSET name1
+	mov	edx, OFFSET name1
 	call	Writestring
 	call	crlf
 	call	crlf
-	mov		edx, OFFSET intro1
+	mov	edx, OFFSET intro1
 	call	Writestring
 	call	crlf
-	mov		edx, OFFSET intro2
+	mov	edx, OFFSET intro2
 	call	Writestring
 	call	crlf
-	mov		edx, OFFSET intro3
+	mov	edx, OFFSET intro3
 	call	Writestring
 	call	crlf
 
@@ -101,29 +101,29 @@ intro ENDP
 ; Post -> the array length is now stores in passed parameter
 ;
 getData	PROC
-	push	ebp					; save the value of ebp
-	mov		ebp, esp			; ebp is now pointing at the top of the stack
-	mov		ebx, [ebp + 8]		; ebp + 8 adress is moved into ebx
+	push	ebp				; save the value of ebp
+	mov	ebp, esp			; ebp is now pointing at the top of the stack
+	mov	ebx, [ebp + 8]			; ebp + 8 adress is moved into ebx
 
 range:
 	mov		edx, OFFSET prompt1
 	call	Writestring
 	call	crlf
 	call	Readint				; readint stores the user input in eax
-	mov		[ebx], eax			; mov eax into the ebx
-	cmp		eax, min			; checks lower limit for validity of user input
-	jl		message
-	cmp		eax, max			; checks upper limit for validity of user input
-	jg		message
+	mov	[ebx], eax			; mov eax into the ebx
+	cmp	eax, min			; checks lower limit for validity of user input
+	jl	message
+	cmp	eax, max			; checks upper limit for validity of user input
+	jg	message
 
-	pop		ebp					; returns the correct value to ebp
-	ret		4					; 4 will remove the top 4 bytes from the stack so that the pushed parameter is no longer there
+	pop	ebp				; returns the correct value to ebp
+	ret	4				; 4 will remove the top 4 bytes from the stack so that the pushed parameter is no longer there
 
-message:						; mesaage will be jumped to if the input is invalid
-	mov		edx, OFFSET invalid
+message:					; mesaage will be jumped to if the input is invalid
+	mov	edx, OFFSET invalid
 	call	Writestring
 	call	crlf
-	jmp		range
+	jmp	range
 getData ENDP
 
 
@@ -136,22 +136,22 @@ getData ENDP
 ;
 fillArray	PROC
 	push	ebp
-	mov		ebp, esp
-	mov		esi, [ebp + 12]
-	mov		ecx, [ebp + 8]
+	mov	ebp, esp
+	mov	esi, [ebp + 12]
+	mov	ecx, [ebp + 8]
 again:
-	mov		eax, hi
-	sub		eax, lo
-	inc		eax					; gets the correct value for the bounds of eax
+	mov	eax, hi
+	sub	eax, lo
+	inc	eax				; gets the correct value for the bounds of eax
 	call	RandomRange			; gets a random number bound by eax
-	add		eax, lo
-	mov		[esi], eax			; esi accesses the element in the array
-	add		esi, 4
+	add	eax, lo
+	mov	[esi], eax			; esi accesses the element in the array
+	add	esi, 4
 
 	loop	again
 
-	pop		ebp					; restore the ebp register
-	ret		8					; removes the values that were put on the top of the stack
+	pop	ebp				; restore the ebp register
+	ret	8				; removes the values that were put on the top of the stack
 fillArray	ENDP
 
 
@@ -163,30 +163,30 @@ fillArray	ENDP
 ;
 median	PROC					; random values need to change each time
 	push	ebp
-	mov		ebp, esp
-	mov		esi, [ebp + 12]
-	mov		ecx, [ebp + 8]
+	mov	ebp, esp
+	mov	esi, [ebp + 12]
+	mov	ecx, [ebp + 8]
 doIt:
-	mov		eax, [esi]
-	add		eax, total				; adds the value in eax to total
-	mov		total, eax				; stores the new total in total
-	add		esi, 4
+	mov	eax, [esi]
+	add	eax, total			; adds the value in eax to total
+	mov	total, eax			; stores the new total in total
+	add	esi, 4
 	loop	doIt
 
 	call	crlf
 	call	crlf
-	mov		edx, OFFSET median1		; prints out median message
+	mov	edx, OFFSET median1		; prints out median message
 	call	Writestring
-	mov		edx, 0
-	mov		eax, total
-	mov		ecx, [ebp + 8]			; moves divisor to ecx
-	DIV		ecx						; stores quotient in eax
-	call	WriteDec				; displays quotient
+	mov	edx, 0
+	mov	eax, total
+	mov	ecx, [ebp + 8]			; moves divisor to ecx
+	DIV	ecx				; stores quotient in eax
+	call	WriteDec			; displays quotient
 	call	crlf
 	call	crlf
 
-	pop		ebp
-	ret		8
+	pop	ebp
+	ret	8
 
 median	ENDP
 
@@ -198,46 +198,46 @@ median	ENDP
 ;
 sortArray PROC
 	push	ebp
-	mov		ebp, esp
-	mov		esi, [ebp + 12]
-	mov		ecx, [ebp + 8]
-	XOR		edx, edx				; since theedx register may hav been used for Writestring before
+	mov	ebp, esp
+	mov	esi, [ebp + 12]
+	mov	ecx, [ebp + 8]
+	XOR	edx, edx			; since theedx register may hav been used for Writestring before
 
-outer:								; go through the array
+outer:						; go through the array
 	push	ecx
-	mov		eax, [esi]				; eax stores the element in the array that you are trying to sort
-	mov		ebx, [esi]
-	push	esi						; puts esi on the stack so the location for the outer loop can be saved also
-	dec		ecx
-	ADD		esi, 4					; gets the next element in the array
+	mov	eax, [esi]			; eax stores the element in the array that you are trying to sort
+	mov	ebx, [esi]
+	push	esi				; puts esi on the stack so the location for the outer loop can be saved also
+	dec	ecx
+	ADD	esi, 4				; gets the next element in the array
 inner:
-	cmp		[esi], eax
-	jg		setter					; if the number is greater set the new eax as the biggest value
+	cmp	[esi], eax
+	jg	setter				; if the number is greater set the new eax as the biggest value
 back:
-	ADD		esi, 4
-	loop	inner					; increments inner loop ecx
+	ADD	esi, 4
+	loop	inner				; increments inner loop ecx
 
 ; exchange the parts of the loop
 
-	mov		[edx], ebx				; swap performed
-	pop		esi						; restore esi
-	mov		ebx, esi				; esi saved to temp value so that it isnt change for the loop
-	mov		[ebx], eax				; other swap performed
+	mov	[edx], ebx			; swap performed
+	pop	esi				; restore esi
+	mov	ebx, esi			; esi saved to temp value so that it isnt change for the loop
+	mov	[ebx], eax			; other swap performed
 
-	ADD		esi, 4					; add 4 tto get next element for outer loop
-	pop		ecx						; restore ecx for outer loop
-	cmp		ecx, 2
-	JE		done
+	ADD	esi, 4				; add 4 tto get next element for outer loop
+	pop	ecx				; restore ecx for outer loop
+	cmp	ecx, 2
+	JE	done
 	loop	outer
 
 done:
-	pop		ebp						; restore the ebp register
-	ret		8
-setter:								; i = j
-	mov		edx, esi
-	mov		eax, [esi]				; stores the location in eax (i)
+	pop	ebp				; restore the ebp register
+	ret	8
+setter:						; i = j
+	mov	edx, esi
+	mov	eax, [esi]			; stores the location in eax (i)
 
-	jmp		back
+	jmp	back
 sortArray ENDP
 
 ;
@@ -248,36 +248,36 @@ sortArray ENDP
 ;
 displayArray	PROC
 	push	ebp
-	mov		ebp, esp
-	mov		edx, [ebp + 16]		; message now in edx
-	mov		esi, [ebp + 12]		; array pointer
-	mov		ecx, [ebp + 8]		; array length
+	mov	ebp, esp
+	mov	edx, [ebp + 16]			; message now in edx
+	mov	esi, [ebp + 12]			; array pointer
+	mov	ecx, [ebp + 8]			; array length
 	call	Writestring			; prints out meessage from edx
 	call	crlf
 more:
-	inc		times				; times stores how many were printed on a line
-	mov		eax, [esi]			; moves value at esi array location to ax
+	inc	times				; times stores how many were printed on a line
+	mov	eax, [esi]			; moves value at esi array location to ax
 	call	Writedec
-	mov		al, tab
+	mov	al, tab
 	call	Writechar
-	add		esi, 4				; adds 4 to esi to access the next element in the Dword array
+	add	esi, 4				; adds 4 to esi to access the next element in the Dword array
 
-	XOR		edx, edx
-	mov		eax, times
-	mov		ebx, 10				; puts 10 in to be divisor
-	DIV		ebx					; divides, times prime by 10
-	cmp		edx, 0				; if remainder is 0, 10 have been printed so there needs to be a new line
-	JE		newline
+	XOR	edx, edx
+	mov	eax, times
+	mov	ebx, 10				; puts 10 in to be divisor
+	DIV	ebx				; divides, times prime by 10
+	cmp	edx, 0				; if remainder is 0, 10 have been printed so there needs to be a new line
+	JE	newline
 back:
 	loop	more
 
-	mov		times, 0
-	pop		ebp					; restore the ebp register
-	ret		8
+	mov	times, 0
+	pop	ebp				; restore the ebp register
+	ret	8
 
 newLine:
 	call	crlf
-	JMP		back
+	JMP	back
 displayArray	ENDP
 
 END main
